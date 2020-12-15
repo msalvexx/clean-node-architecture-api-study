24a4f5df5331a2f278804b99c76462ece51baa82
Matheus Alves Arruda <matheus.arruda@radixeng.com.br>
2020-12-14 21:20:30 -0300

    refactor: simplifica codigo

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -4,14 +4,12 @@ import { HttpRequest, HttpResponse } from '../protocols/http'
 
 export class SignUpController {
   handle (httpRequest: HttpRequest): HttpResponse {
-    if (!httpRequest.body.name) {
-      return badRequest(new MissingParameterError('name'))
+    const requiredFields = ['name', 'email']
+    for (const field of requiredFields) {
+      if (!httpRequest.body[field]) {
+        return badRequest(new MissingParameterError(field))
+      }
     }
-
-    if (!httpRequest.body.email) {
-      return badRequest(new MissingParameterError('email'))
-    }
-
     return {
       statusCode: 200,
       body: 'ok'
