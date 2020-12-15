b65fadd2fc1280528d9660ea8ac3cf143089ba5d
Matheus Alves Arruda <matheus.arruda@radixeng.com.br>
2020-12-14 21:16:41 -0300

    refactor: move codigo duplicado para metodo utilitario

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,20 +1,15 @@
 import { MissingParameterError } from '../errors/missing-parameter-error'
+import { badRequest } from '../helpers/http-helper'
 import { HttpRequest, HttpResponse } from '../protocols/http'
 
 export class SignUpController {
   handle (httpRequest: HttpRequest): HttpResponse {
     if (!httpRequest.body.name) {
-      return {
-        statusCode: 400,
-        body: new MissingParameterError('name')
-      }
+      return badRequest(new MissingParameterError('name'))
     }
 
     if (!httpRequest.body.email) {
-      return {
-        statusCode: 400,
-        body: new MissingParameterError('email')
-      }
+      return badRequest(new MissingParameterError('email'))
     }
 
     return {
