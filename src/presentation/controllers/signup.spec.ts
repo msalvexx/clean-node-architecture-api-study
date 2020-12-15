c45e57757efd9d6c4130e69a61ea2f1ace629c77
Matheus Alves Arruda <matheus.arruda@radixeng.com.br>
2020-12-14 21:23:27 -0300

    test: certifica que SignUpController retorna erro 400 se nenhuma senha informada

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -32,3 +32,19 @@ describe('SignUp Controller', () => {
     expect(httpResponse.body).toEqual(new MissingParameterError('email'))
   })
 })
+
+describe('SignUp Controller', () => {
+  test('Should return 400 if no password is provided', () => {
+    const sut = new SignUpController()
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        email: 'any_email@email.com',
+        passwordConfirmation: 'any_password'
+      }
+    }
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new MissingParameterError('password'))
+  })
+})
