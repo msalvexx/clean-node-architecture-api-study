b5af84369e653973bd2e53f301e65ca9cf679826
Matheus Alves Arruda <matheus.arruda@radixeng.com.br>
2020-12-16 07:42:17 -0300

    feat: certifica que o SignUpController retorna erro 400 se o parametro email é invalido

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,15 +1,24 @@
+import { InvalidParameterError } from '../errors/invalid-parameter-error'
 import { MissingParameterError } from '../errors/missing-parameter-error'
 import { badRequest } from '../helpers/http-helper'
 import { Controller } from '../protocols/controller'
+import { EmailValidator } from '../protocols/email-validator'
 import { HttpRequest, HttpResponse } from '../protocols/http'
 
 export class SignUpController implements Controller {
+  constructor (private readonly emailValidator: EmailValidator) {
+  }
+
   handle (httpRequest: HttpRequest): HttpResponse {
     const requiredFields = ['name', 'email', 'password', 'passwordConfirmation']
     for (const field of requiredFields) {
       if (!httpRequest.body[field]) {
         return badRequest(new MissingParameterError(field))
       }
+    }
+    const isValid = this.emailValidator.isValid(httpRequest.body.email)
+    if (!isValid) {
+      return badRequest(new InvalidParameterError('email'))
     }
     return {
       statusCode: 200,
