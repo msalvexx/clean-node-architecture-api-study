85cc57a610fc7a6699b99e8e41ce3140ed90ab85
Matheus Alves Arruda <matheus.arruda@radixeng.com.br>
2020-12-18 21:19:43 -0300

    refactor: remove codigo duplicado

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -14,10 +14,11 @@ export class SignUpController implements Controller {
           return badRequest(new MissingParameterError(field))
         }
       }
-      if (httpRequest.body.password !== httpRequest.body.passwordConfirmation) {
+      const { email, password, passwordConfirmation } = httpRequest.body
+      if (password !== passwordConfirmation) {
         return badRequest(new InvalidParameterError('passwordConfirmation'))
       }
-      const isValid = this.emailValidator.isValid(httpRequest.body.email)
+      const isValid = this.emailValidator.isValid(email)
       if (!isValid) {
         return badRequest(new InvalidParameterError('email'))
       }
