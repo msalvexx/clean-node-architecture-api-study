f31c9d294757c542bec9d05c572dad8098868ea9
Matheus Alves Arruda <matheus.arruda@radixeng.com.br>
2020-12-18 21:16:50 -0300

    feat: certifica que SignupController retorna erro 400 se confirmação de senha falhar

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -14,6 +14,9 @@ export class SignUpController implements Controller {
           return badRequest(new MissingParameterError(field))
         }
       }
+      if (httpRequest.body.password !== httpRequest.body.passwordConfirmation) {
+        return badRequest(new InvalidParameterError('passwordConfirmation'))
+      }
       const isValid = this.emailValidator.isValid(httpRequest.body.email)
       if (!isValid) {
         return badRequest(new InvalidParameterError('email'))
