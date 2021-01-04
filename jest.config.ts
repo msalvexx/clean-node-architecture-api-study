1ba9c7c742b9a4ad60ffbc2f654b5659916e59c5
Matheus Alves Arruda <matheus.arruda@radixeng.com.br>
2021-01-04 01:39:36 -0300

    chore: adiciona scripts e configurações do jest

--- jest.config.ts
+++ jest.config.ts
@@ -1,9 +1,9 @@
-export default {
+module.exports = {
   roots: ['<rootDir>/src'],
   collectCoverageFrom: [
-    '<rootDir>/src/**/.*ts',
+    '<rootDir>/src/**/*.ts',
     '!<rootDir>/src/main/**',
-    '!<rootDir>/src/**/*-protocols.ts',
+    '!<rootDir>/src/**/*protocols.ts',
     '!**/protocols/**',
     '!**/test/**'
   ],
