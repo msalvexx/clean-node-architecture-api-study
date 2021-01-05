dfe80ecb66cc2066e2281ac3cce72b5c0e804b73
Matheus Alves Arruda <matheus.arruda@radixeng.com.br>
2021-01-04 21:50:54 -0300

    feat: certifica que o DbAddAccount chama o Encrypter com a senha válida

---
+++ src/data/usecases/add-account/db-add-account.ts
@@ -0,0 +1,13 @@
+import { Account } from '../../../domain/models/account'
+import { AddAccount, AddAccountModel } from '../../../domain/usecases/add-account'
+import { Encrypter } from '../../protocols/encrypter'
+
+export class DbAddAccount implements AddAccount {
+  constructor (private readonly encrypter: Encrypter) {
+  }
+
+  async add (account: AddAccountModel): Promise<Account> {
+    await this.encrypter.encrypt(account.password)
+    return new Promise(resolve => resolve(null))
+  }
+}
