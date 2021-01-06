beb41a73b82b2f5f1a0458731ea311d50867a9f2
Matheus Alves Arruda <matheus.arruda@radixeng.com.br>
2021-01-06 20:33:14 -0300

    test: certifica que o BcryptAdapter retorna a hash em caso de sucesso

--- src/infra/criptography/bcrypt-adapter.spec.ts
+++ src/infra/criptography/bcrypt-adapter.spec.ts
@@ -1,6 +1,12 @@
 import bcrypt from 'bcrypt'
 import { BCryptAdapter } from './bcrypt-adapter'
 
+jest.mock('bcrypt', () => ({
+  async hash (): Promise<string> {
+    return new Promise(resolve => resolve('hashed_value'))
+  }
+}))
+
 describe('BCrypt Adapter', () => {
   test('Should call encrypt with correct values', async () => {
     const salt = 12
@@ -8,5 +14,12 @@ describe('BCrypt Adapter', () => {
     const hashSpy = jest.spyOn(bcrypt, 'hash')
     await sut.encrypt('any_value')
     expect(hashSpy).toHaveBeenCalledWith('any_value', salt)
+  })
+
+  test('Should return hash on success', async () => {
+    const salt = 12
+    const sut = new BCryptAdapter(salt)
+    const hashedValue = await sut.encrypt('any_value')
+    expect(hashedValue).toBe('hashed_value')
   })
 })
