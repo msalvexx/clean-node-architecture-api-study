73f373d463a8d71fad23824853999a49034c4ca5
Matheus Alves Arruda <matheus.arruda@radixeng.com.br>
2021-01-06 19:51:50 -0300

    feat: certifica que DbAddAccount chama o método add do AddAccountRepository com os valores corretos

--- src/data/usecases/add-account/db-add-account.ts
+++ src/data/usecases/add-account/db-add-account.ts
@@ -1,11 +1,18 @@
 import { Encrypter, Account, AddAccount, AddAccountModel } from './db-add-account.protocols'
+import { AddAccountRepository } from '../../protocols/add-account-repository'
 
 export class DbAddAccount implements AddAccount {
-  constructor (private readonly encrypter: Encrypter) {
-  }
+  constructor (
+    private readonly encrypter: Encrypter,
+    private readonly addAccountRepository: AddAccountRepository
+  ) {}
 
   async add (account: AddAccountModel): Promise<Account> {
-    await this.encrypter.encrypt(account.password)
+    const hashedPassword = await this.encrypter.encrypt(account.password)
+    await this.addAccountRepository.add({
+      ...account,
+      password: hashedPassword
+    })
     return new Promise(resolve => resolve(null))
   }
 }
