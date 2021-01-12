9129c4e0825d60afedb44b7d9a9dfeb6e2afb552
Matheus Alves Arruda <matheus.arruda@radixeng.com.br>
2021-01-12 10:08:51 -0300

    test: ensure LogControllerDecorator returns the same result of the Controller

--- src/main/decorators/log/log.spec.ts
+++ src/main/decorators/log/log.spec.ts
@@ -42,4 +42,26 @@ describe('Log decorator', () => {
     await sut.handle(httpRequest)
     expect(handleSpy).toHaveBeenCalledWith(httpRequest)
   })
+
+  test('Should return the same result of the controller', async () => {
+    const { sut } = makeSut()
+    const httpRequest: HttpRequest = {
+      body: {
+        name: 'any_name',
+        email: 'any_email@mail.com',
+        password: 'any_password',
+        passwordConfirmation: 'any_password'
+      }
+    }
+    const httpResponse = await sut.handle(httpRequest)
+    expect(httpResponse).toEqual({
+      statusCode: 200,
+      body: {
+        id: 'any_id',
+        name: 'any_name',
+        email: 'any_email@mail.com',
+        password: 'any_password'
+      }
+    })
+  })
 })
