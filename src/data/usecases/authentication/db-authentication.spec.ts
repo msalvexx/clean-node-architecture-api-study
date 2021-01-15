cf40ac8e749e139646675e91475bd3949d3f8d16
Matheus Alves Arruda <matheus.arruda@radixeng.com.br>
2021-01-14 23:15:56 -0300

    refactor: move request to a factory method

--- src/data/usecases/authentication/db-authentication.spec.ts
+++ src/data/usecases/authentication/db-authentication.spec.ts
@@ -1,4 +1,5 @@
 import { Account } from '../../../domain/models/account'
+import { AuthenticationModel } from '../../../domain/usecases/authentication'
 import { LoadAccountByEmailRepository } from '../../protocols/load-account-by-email-repository'
 import { DbAuthentication } from './db-authentication'
 
@@ -31,14 +32,16 @@ const makeSut = (): SutTypes => {
   }
 }
 
+const makeFakeCredential = (): AuthenticationModel => ({
+  email: 'any_email@mail.com',
+  password: 'any_password'
+})
+
 describe('DbAuthentication', () => {
   test('Should call LoadAccountByEmailRepository with correct email ', async () => {
     const { sut, loadAccountByEmailRepository } = makeSut()
     const loadSpy = jest.spyOn(loadAccountByEmailRepository, 'load')
-    await sut.auth({
-      email: 'any_email@mail.com',
-      password: 'any_password'
-    })
+    await sut.auth(makeFakeCredential())
     expect(loadSpy).toHaveBeenCalledWith('any_email@mail.com')
   })
 })
