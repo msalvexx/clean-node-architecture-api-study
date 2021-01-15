dc9351e97ad2e53a3a0bf4990afdf19843e685b0
Matheus Alves Arruda <matheus.arruda@radixeng.com.br>
2021-01-15 09:37:47 -0300

    test: ensure DbAuthentication throws if TokenGenerator throws

--- src/data/usecases/authentication/db-authentication.spec.ts
+++ src/data/usecases/authentication/db-authentication.spec.ts
@@ -114,4 +114,11 @@ describe('DbAuthentication', () => {
     await sut.auth(makeFakeCredential())
     expect(generateSpy).toHaveBeenCalledWith('any_id')
   })
+
+  test('Should throw if TokenGenerator throws ', async () => {
+    const { sut, tokenGeneratorStub } = makeSut()
+    jest.spyOn(tokenGeneratorStub, 'generate').mockReturnValueOnce(new Promise((resolve, reject) => reject(new Error())))
+    const promise = sut.auth(makeFakeCredential())
+    await expect(promise).rejects.toThrow()
+  })
 })
