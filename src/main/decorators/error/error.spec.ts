0afa95f6ea238a6594aa5e10c99cba999d73ca36
Matheus Alves Arruda <matheus.arruda@radixeng.com.br>
2021-01-15 20:40:23 -0300

    test: ensure DefaultErrorControllerDecorator calls Controller with correct value

---
+++ src/main/decorators/error/error.spec.ts
@@ -0,0 +1,49 @@
+import { ok } from '../../../presentation/helpers/http/http-helper'
+import { Controller, HttpRequest, HttpResponse } from '../../../presentation/protocols'
+import { Account } from '../../../domain/models/account'
+import { DefaultErrorControllerDecorator } from './error'
+
+interface SutTypes {
+  sut: DefaultErrorControllerDecorator
+  controllerStub: Controller
+}
+
+const makeControllerStub = (): Controller => {
+  class ControllerStub implements Controller {
+    async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
+      return new Promise(resolve => resolve(ok(makeFakeAccount())))
+    }
+  }
+  return new ControllerStub()
+}
+
+const makeSut = (): SutTypes => {
+  const controllerStub = makeControllerStub()
+  const sut = new DefaultErrorControllerDecorator(controllerStub)
+  return { sut, controllerStub }
+}
+
+const makeFakeAccount = (): Account => ({
+  id: 'any_id',
+  name: 'any_name',
+  email: 'any_email@mail.com',
+  password: 'any_password'
+})
+
+const makeFakeRequest = (): HttpRequest => ({
+  body: {
+    name: 'any_name',
+    email: 'any_email@mail.com',
+    password: 'any_password',
+    passwordConfirmation: 'any_password'
+  }
+})
+
+describe('Controller Decorator', () => {
+  test('Should call default controller handle', async () => {
+    const { sut, controllerStub } = makeSut()
+    const handleSpy = jest.spyOn(controllerStub, 'handle')
+    await sut.handle(makeFakeRequest())
+    expect(handleSpy).toHaveBeenCalledWith(makeFakeRequest())
+  })
+})
