1fd4e4cf85e4ff4d0221231d43c9d48f4e83a56e
Matheus Alves Arruda <matheus.arruda@radixeng.com.br>
2021-01-17 09:44:31 -0300

    feat: ensure loadByEmail return an account on success

--- src/infra/db/mongodb/account-repository/account.ts
+++ src/infra/db/mongodb/account-repository/account.ts
@@ -1,9 +1,16 @@
 import { AddAccountRepository } from '../../../../data/protocols/db/account/add-account-repository'
+import { LoadAccountByEmailRepository } from '../../../../data/usecases/authentication/db-authentication.protocols'
 import { Account } from '../../../../domain/models/account'
 import { AddAccountModel } from '../../../../domain/usecases/add-account'
 import { MongoHelper } from '../helpers/mongo.helper'
 
-export class AccountMongoRepository implements AddAccountRepository {
+export class AccountMongoRepository implements AddAccountRepository, LoadAccountByEmailRepository {
+  async loadByEmail (email: string): Promise<Account> {
+    const accountCollection = await MongoHelper.getCollection('accounts')
+    const account = await accountCollection.findOne({ email })
+    return MongoHelper.map(account)
+  }
+
   async add (accountModel: AddAccountModel): Promise<Account> {
     const accountCollection = await MongoHelper.getCollection('accounts')
     const result = await accountCollection.insertOne(accountModel)
