a580f14f84b1429f61a36bf846279877ce4f685f
Matheus Alves Arruda <matheus.arruda@radixeng.com.br>
2021-01-18 11:40:18 -0300

    test: add login route

--- src/main/routes/auth-routes/auth-routes.test.ts
+++ src/main/routes/auth-routes/auth-routes.test.ts
@@ -1,6 +1,10 @@
+import { Collection } from 'mongodb'
 import request from 'supertest'
 import { MongoHelper } from '../../../infra/db/mongodb/helpers/mongo.helper'
 import app from '../../config/app'
+import bcrypt from 'bcrypt'
+
+let accountCollection: Collection
 
 describe('Authentication Routes', () => {
   beforeAll(async () => {
@@ -12,7 +16,7 @@ describe('Authentication Routes', () => {
   })
 
   beforeEach(async () => {
-    const accountCollection = await MongoHelper.getCollection('accounts')
+    accountCollection = await MongoHelper.getCollection('accounts')
     await accountCollection.deleteMany({})
   })
 
@@ -25,6 +29,24 @@ describe('Authentication Routes', () => {
           email: 'matheus@email.com',
           password: '123',
           passwordConfirmation: '123'
+        })
+        .expect(200)
+    })
+  })
+
+  describe('POST /login', () => {
+    test('Should return 200 on login', async () => {
+      const password = await bcrypt.hash('123', 12)
+      await accountCollection.insertOne({
+        name: 'Matheus',
+        email: 'matheus@email.com',
+        password: password
+      })
+      await request(app)
+        .post('/api/login')
+        .send({
+          email: 'matheus@email.com',
+          password: '123'
         })
         .expect(200)
     })
