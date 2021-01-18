39e8b4224878ce19affb67499bea0112fe92891b
Matheus Alves Arruda <matheus.arruda@radixeng.com.br>
2021-01-18 11:39:17 -0300

    feat: add login factory

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,4 +1,5 @@
 export default {
   mongoUrl: process.env.MONGO_URL || 'mongodb://localhost:27017/clean-node-api',
-  port: 5050
+  port: 5050,
+  jwtSecret: process.env.JWT_SECRET || 'mIlao(1$0.!LL2F'
 }
