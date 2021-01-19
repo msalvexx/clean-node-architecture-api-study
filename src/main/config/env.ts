014befbf1dfe6c1b69be646d827c30404a6fb09b
Matheus Alves Arruda <matheus.arruda@radixeng.com.br>
2021-01-19 12:06:14 -0300

    chore: fix port env var

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,5 +1,5 @@
 export default {
   mongoUrl: process.env.MONGO_URL || 'mongodb://localhost:27017/clean-node-api',
-  port: 5050,
+  port: process.env.PORT || 5050,
   jwtSecret: process.env.JWT_SECRET || 'mIlao(1$0.!LL2F'
 }
