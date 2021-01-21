8242e0ce0d96d651815410ac0b0fa78b7366055c
Matheus Alves Arruda <matheus.arruda@radixeng.com.br>
2021-01-21 12:52:54 -0300

    feat: add DbAuthentication on SignupController factory

--- src/main/factories/signup/signup-factory.ts
+++ src/main/factories/signup/signup-factory.ts
@@ -1,9 +1,12 @@
 import { DbAddAccount } from '../../../data/usecases/add-account/db-add-account'
+import { DbAuthentication } from '../../../data/usecases/authentication/db-authentication'
 import { BCryptAdapter } from '../../../infra/criptography/bcrypt-adapter/bcrypt-adapter'
+import { JwtAdapter } from '../../../infra/criptography/jwt-adapter/jwt-adapter'
 import { AccountMongoRepository } from '../../../infra/db/mongodb/account/account-mongo-repository'
 import { LogMongoRepository } from '../../../infra/db/mongodb/log/log-mongo-repository'
 import { SignUpController } from '../../../presentation/controllers/signup/signup-controller'
 import { Controller } from '../../../presentation/protocols'
+import env from '../../config/env'
 import { DefaultErrorControllerDecorator } from '../../decorators/error/error'
 import { LogControllerDecorator } from '../../decorators/log/log-controller-decorator'
 import { makeSignUpValidation } from './signup-validation-factory'
@@ -14,7 +17,11 @@ export const makeSignUpController = (): Controller => {
   const addAccountRepository = new AccountMongoRepository()
   const addAccount = new DbAddAccount(hasher, addAccountRepository)
   const validationComposite = makeSignUpValidation()
-  const signUpController = new SignUpController(addAccount, validationComposite)
+  const accountRepository = new AccountMongoRepository()
+  const hashComparer = new BCryptAdapter(salt)
+  const token = new JwtAdapter(env.jwtSecret)
+  const authentication = new DbAuthentication(accountRepository, hashComparer, token, accountRepository)
+  const signUpController = new SignUpController(addAccount, validationComposite, authentication)
   const defaultErrorControllerDecorator = new DefaultErrorControllerDecorator(signUpController)
   const logMongoRepository = new LogMongoRepository()
   return new LogControllerDecorator(defaultErrorControllerDecorator, logMongoRepository)
