5a62fca81ed71507c76968bb856ed4ffe071a5da
Matheus Alves Arruda <matheus.arruda@radixeng.com.br>
2021-01-21 15:41:28 -0300

    feat: ensure SignUpController ValidationComposite contains UniqueEmailValidation

--- src/main/factories/signup/signup-validation-factory.ts
+++ src/main/factories/signup/signup-validation-factory.ts
@@ -1,14 +1,17 @@
-import { CompareFieldsValidation, EmailValidation, ValidationComposite } from '../../../presentation/helpers/validators'
+import { AccountMongoRepository } from '../../../infra/db/mongodb/account/account-mongo-repository'
+import { CompareFieldsValidation, EmailValidation, ValidationComposite, UniqueEmailValidation } from '../../../presentation/helpers/validators'
 import { Validation } from '../../../presentation/protocols/validation'
 import { EmailValidatorAdapter } from '../../adapters/validators/email-validator-adapter'
 import { makeRequiredFieldsValidators } from '../validators/required-validators'
 
 export const makeSignUpValidation = (): ValidationComposite => {
   const emailValidator = new EmailValidatorAdapter()
+  const assertAccountExistsByEmailRepository = new AccountMongoRepository()
   const validations: Validation[] = [
     ...makeRequiredFieldsValidators(['name', 'email', 'password', 'passwordConfirmation']),
     new CompareFieldsValidation('password', 'passwordConfirmation'),
-    new EmailValidation('email', emailValidator)
+    new EmailValidation('email', emailValidator),
+    new UniqueEmailValidation('email', assertAccountExistsByEmailRepository)
   ]
   return new ValidationComposite(validations)
 }
