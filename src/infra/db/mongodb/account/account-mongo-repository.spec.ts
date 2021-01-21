4b039565b6e422c72f282c057545a48d832c05d4
Matheus Alves Arruda <matheus.arruda@radixeng.com.br>
2021-01-21 15:25:45 -0300

    test: ensure exists return false if not exists any account associated with the email

--- src/infra/db/mongodb/account/account-mongo-repository.spec.ts
+++ src/infra/db/mongodb/account/account-mongo-repository.spec.ts
@@ -76,10 +76,16 @@ describe('Account Mongo Repository', () => {
     expect(account.accessToken).toBe('any_token')
   })
 
-  test('Should return true if email already exists', async () => {
+  test('Should return true if email already exists in collection', async () => {
     const sut = makeSut()
     await accountCollection.insertOne(makeFakeAccount())
     const result = await sut.exists('any_email@email.com')
     expect(result).toBeTruthy()
+  })
+
+  test('Should return false if email not exists in collection', async () => {
+    const sut = makeSut()
+    const result = await sut.exists('any_email@email.com')
+    expect(result).toBeFalsy()
   })
 })
