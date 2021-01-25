7ef42774fb7fe027c12d156f770d245f0bf1f712
Matheus Alves Arruda <matheus.arruda@radixeng.com.br>
2021-01-25 18:44:00 -0300

    feat: ensure DbLoadAccountByToken throw AccessDeniedError if decrypt fails

--- src/data/usecases/load-account-by-token/load-account-by-token.ts
+++ src/data/usecases/load-account-by-token/load-account-by-token.ts
@@ -1,3 +1,5 @@
+import { AccessDeniedError } from "../../../domain/errors/access-denied-error";
+import { InvalidCredentialsError } from "../../../domain/errors/invalid-credentials-error";
 import { Account } from "../../../domain/models/account";
 import { LoadAccountByToken } from "../../../domain/usecases/load-account-by-token";
 import { Decrypter } from "../../protocols/criptography/decrypter";
@@ -6,7 +8,13 @@ export class DbLoadAccountByToken implements LoadAccountByToken {
   constructor (private readonly decrypter: Decrypter) { }
 
   async load (accessToken: string, role?: string): Promise<Account> {
-    await this.decrypter.decrypt(accessToken)
+    try {
+      await this.decrypter.decrypt(accessToken)
+    } catch (e) {
+      if (e instanceof InvalidCredentialsError) {
+        throw new AccessDeniedError()
+      }
+    }
     return null
   }
 }
