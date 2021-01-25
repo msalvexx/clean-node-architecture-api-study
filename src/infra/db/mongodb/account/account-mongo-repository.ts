21de80f455b9ed4ff04773a1b5b5209b6cee7158
Matheus Alves Arruda <matheus.arruda@radixeng.com.br>
2021-01-25 20:42:28 -0300

    feat: filter by role

--- src/infra/db/mongodb/account/account-mongo-repository.ts
+++ src/infra/db/mongodb/account/account-mongo-repository.ts
@@ -9,7 +9,7 @@ import { MongoHelper } from '../helpers/mongo.helper'
 export class AccountMongoRepository implements AddAccountRepository, LoadAccountByEmailRepository, UpdateAccessTokenRepository, ExistsRegisterInRepository, LoadAccountByTokenRepository {
   async loadByToken (token: string, role?: string): Promise<Account> {
     const accountCollection = await MongoHelper.getCollection('accounts')
-    const account = await accountCollection.findOne({ accessToken: token })
+    const account = await accountCollection.findOne({ accessToken: token, role })
     this.throwNotFoundWhenNull(account)
     return Promise.resolve(null)
   }
