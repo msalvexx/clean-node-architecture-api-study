fe71b39a39bea2091f87dcc5f88a3efb4c83d0f8
Matheus Alves Arruda <matheus.arruda@radixeng.com.br>
2021-01-25 18:21:21 -0300

    feat: ensure DbLoadAccountByToken calls decrypter with the correct token

---
+++ src/data/usecases/load-account-by-token/load-account-by-token.ts
@@ -0,0 +1,12 @@
+import { Account } from "../../../domain/models/account";
+import { LoadAccountByToken } from "../../../domain/usecases/load-account-by-token";
+import { Decrypter } from "../../protocols/criptography/decrypter";
+
+export class DbLoadAccountByToken implements LoadAccountByToken {
+  constructor (private readonly decrypter: Decrypter) { }
+
+  async load (accessToken: string, role?: string): Promise<Account> {
+    await this.decrypter.decrypt(accessToken)
+    return null
+  }
+}
