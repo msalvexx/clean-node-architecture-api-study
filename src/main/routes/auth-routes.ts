d4762714feacd67c5d1d96dca8743952f50a617d
Matheus Alves Arruda <matheus.arruda@radixeng.com.br>
2021-01-25 20:59:43 -0300

    refactor: rename file name

--- src/main/routes/auth-routes.ts
+++ src/main/routes/auth-routes.ts
@@ -1,7 +1,7 @@
 import { Router } from 'express'
 import { makeLoginController } from '../factories/controllers/login/login-controller-factory'
 import { makeSignUpController } from '../factories/controllers/signup/signup-controller-factory'
-import { adaptRoute } from '../adapters/express/express-route-adapters'
+import { adaptRoute } from '../adapters/express/express-route-adapter'
 
 export default (router: Router): void => {
   router.post('/signup', adaptRoute(makeSignUpController()))
--- src/main/routes/survey-routes.ts
+++ src/main/routes/survey-routes.ts
@@ -1,5 +1,5 @@
 import { Router } from 'express'
-import { adaptRoute } from '../adapters/express/express-route-adapters'
+import { adaptRoute } from '../adapters/express/express-route-adapter'
 import { makeAddSurveyController } from '../factories/controllers/add-survey/add-survey-controller-factory'
 
 export default (router: Router): void => {
