f75c757c6cabaa0ef58a74ee4c4226278c613437
Matheus Alves Arruda <matheus.arruda@radixeng.com.br>
2021-01-31 14:36:52 -0300

    feat: should request the surveys founded on success

--- src/presentation/controllers/survey/load-surveys/load-surveys-controller.ts
+++ src/presentation/controllers/survey/load-surveys/load-surveys-controller.ts
@@ -1,10 +1,11 @@
+import { ok } from '../../../helpers/http/http-helper'
 import { LoadSurveys, Controller, HttpRequest, HttpResponse } from './load-surveys-controller.protocols'
 
 export class LoadSurveysController implements Controller {
   constructor (private readonly surveys: LoadSurveys) { }
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    await this.surveys.load()
-    return Promise.resolve(null)
+    const surveys = await this.surveys.load()
+    return ok(surveys)
   }
 }
