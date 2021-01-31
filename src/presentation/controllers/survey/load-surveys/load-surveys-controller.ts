7e822f194f0c684b55e27247683a2cfd94acf524
Matheus Alves Arruda <matheus.arruda@radixeng.com.br>
2021-01-31 15:09:54 -0300

    feat: ensure LoadSurveysController returns 204 if no survey is founded

--- src/presentation/controllers/survey/load-surveys/load-surveys-controller.ts
+++ src/presentation/controllers/survey/load-surveys/load-surveys-controller.ts
@@ -1,4 +1,4 @@
-import { ok } from '../../../helpers/http/http-helper'
+import { noContent, ok } from '../../../helpers/http/http-helper'
 import { LoadSurveys, Controller, HttpRequest, HttpResponse } from './load-surveys-controller.protocols'
 
 export class LoadSurveysController implements Controller {
@@ -6,6 +6,6 @@ export class LoadSurveysController implements Controller {
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     const surveys = await this.surveys.load()
-    return ok(surveys)
+    return surveys.length ? ok(surveys) : noContent()
   }
 }
