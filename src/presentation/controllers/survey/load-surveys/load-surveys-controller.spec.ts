c6165687aadc77f7fa941172e700f2c9788dc079
Matheus Alves Arruda <matheus.arruda@radixeng.com.br>
2021-01-31 14:37:47 -0300

    test: should request the surveys founded on success

--- src/presentation/controllers/survey/load-surveys/load-surveys-controller.spec.ts
+++ src/presentation/controllers/survey/load-surveys/load-surveys-controller.spec.ts
@@ -1,5 +1,7 @@
 import { LoadSurveys, Survey } from './load-surveys-controller.protocols'
 import { LoadSurveysController } from './load-surveys-controller'
+import { ok } from '../../../helpers/http/http-helper'
+import MockDate from 'mockdate'
 
 interface SutTypes {
   sut: LoadSurveysController
@@ -42,10 +44,24 @@ const makeSut = (): SutTypes => {
 }
 
 describe('LoadSurveysController', () => {
+  beforeAll(() => {
+    MockDate.set(new Date())
+  })
+
+  afterAll(() => {
+    MockDate.reset()
+  })
+
   test('Should call LoadSurveys.load', async () => {
     const { sut, loadSurveyStub } = makeSut()
     const loadSpy = jest.spyOn(loadSurveyStub, 'load')
     await sut.handle({})
     expect(loadSpy).toHaveBeenCalled()
+  })
+
+  test('Should return the surveys on success', async () => {
+    const { sut } = makeSut()
+    const result = await sut.handle({})
+    expect(result).toStrictEqual(ok(makeFakeSurveys()))
   })
 })
